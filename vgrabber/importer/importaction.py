a6f8a8be951468106e9b9a9c41e14137fa2f412c
Jan Janech <janik@janik.ws>
2018-08-24 14:58:54 +0200

    import actions dependencies fixed

--- vgrabber/importer/importaction.py
+++ vgrabber/importer/importaction.py
@@ -19,7 +19,7 @@ ImportAction.grades.depends = (ImportAction.final_exam_list, ImportAction.studen
 ImportAction.moodle_student_list.depends = (ImportAction.student_list, )
 ImportAction.moodle_home_work_list.depends = (ImportAction.moodle_student_list, )
 ImportAction.moodle_home_work_grades.depends = (ImportAction.moodle_home_work_list, )
-ImportAction.moodle_home_work_details.depends = (ImportAction.moodle_home_work_details, )
+ImportAction.moodle_home_work_details.depends = (ImportAction.moodle_home_work_grades, )
 ImportAction.moodle_final_exam_list.depends = (ImportAction.moodle_student_list, ImportAction.final_exam_list)
 ImportAction.moodle_final_exam_details.depends = (ImportAction.moodle_final_exam_list, )
 
