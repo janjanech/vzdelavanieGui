84684922e493bb4669d363b7056c2fe8a1fab3f1
Jan Janech <janik@janik.ws>
2019-01-14 17:32:45 +0100

    stored file's content  can be shown inapp if the type is supported

--- vgrabber/qtgui/tabs/helpers/childfileitems.py
+++ vgrabber/qtgui/tabs/helpers/childfileitems.py
@@ -10,16 +10,13 @@ fip = QFileIconProvider()
 
 def add_file_items(files, parent_item: QTreeWidgetItem):
     for file in files:
-        if isinstance(file, StoredFile):
-            file_item = FileItem([file.file_name], file)
-        else:
-            file_item = QTreeWidgetItem([file.file_name])
+        file_item = FileItem([file.file_name], file)
         file_item.setIcon(0, fip.icon(QFileInfo(file.file_name)))
         parent_item.addChild(file_item)
         file_item.setFirstColumnSpanned(True)
 
 
 def file_double_clicked(model, item):
-    if isinstance(item, FileItem):
+    if isinstance(item, FileItem) and isinstance(item.file, StoredFile):
         file_path = model.data_layer.open_file_for_external_app(item.file.file_path)
         QDesktopServices.openUrl(QUrl.fromLocalFile(file_path))
